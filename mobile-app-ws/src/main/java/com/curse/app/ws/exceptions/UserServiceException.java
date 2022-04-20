9e5f6208bbe2d16a8cbece059c3d9b8139a265d9
Andres Vallecilla <andresfelipe083195@hotmail.com>
2022-04-19 20:19:36 -0500

    [create] Exceptions personalized

---
+++ mobile-app-ws/src/main/java/com/curse/app/ws/exceptions/UserServiceException.java
@@ -0,0 +1,15 @@
+package com.curse.app.ws.exceptions;
+
+public class UserServiceException extends RuntimeException {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6136708106865709822L;
+
+	public UserServiceException(String message)
+	{
+		super(message);
+	}
+
+}
