3d4c6366ce3282e380a75c64b375bc9e126596b6
Andres Vallecilla <andresfelipe083195@hotmail.com>
2022-04-21 20:29:49 -0500

    [create] example eureka api

---
+++ Eureka/PhotoAppDiscoveryService/src/test/java/com/curse/app/ws/PhotoAppDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curse.app.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
