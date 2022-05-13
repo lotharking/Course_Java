e6e0b95635a59c7bb5b8f2b38ea66abf7016ae4d
Andres Vallecilla <andresfelipe083195@hotmail.com>
2022-05-12 23:39:51 -0500

    [create] users details structure

---
+++ Eureka/PhotoAppApiUsers/src/main/java/com/curse/app/ws/photoapp/api/users/service/UsersService.java
@@ -0,0 +1,9 @@
+package com.curse.app.ws.photoapp.api.users.service;
+
+import com.curse.app.ws.photoapp.api.users.shared.UserDto;
+
+public interface UsersService {
+	
+	UserDto createUser(UserDto userDetails);
+
+}
---
+++ Eureka/PhotoAppApiUsers/src/main/java/com/curse/app/ws/photoapp/api/users/service/UsersServiceImpl.java
@@ -0,0 +1,41 @@
+package com.curse.app.ws.photoapp.api.users.service;
+
+import java.util.UUID;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.curse.app.ws.photoapp.api.users.data.UserEntity;
+import com.curse.app.ws.photoapp.api.users.data.UsersRepository;
+import com.curse.app.ws.photoapp.api.users.shared.UserDto;
+
+@Service
+public class UsersServiceImpl implements UsersService {
+	
+	UsersRepository usersRepository;
+
+	@Autowired
+	public UsersServiceImpl(UsersRepository usersRepository)
+	{
+		this.usersRepository = usersRepository;
+	}
+
+	@Override
+	public UserDto createUser(UserDto userDetails) {
+		// TODO Auto-generated method stub
+		
+		userDetails.setUserId(UUID.randomUUID().toString());
+		ModelMapper modelMapper = new ModelMapper();
+		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+		
+		UserEntity userEntity = modelMapper.map(userDetails, UserEntity.class);
+		userEntity.setEncryptedPassword("test");
+		
+		usersRepository.save(userEntity);
+		
+		return null;
+	}
+
+}
