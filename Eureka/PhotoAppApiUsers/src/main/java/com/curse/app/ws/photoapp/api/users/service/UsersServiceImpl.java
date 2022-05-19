e6b2eec4160a21b82adeb03c4436efe6a52554b1
Andres Vallecilla <andresfelipe083195@hotmail.com>
2022-05-18 23:47:37 -0500

    [change] configure security and encrypted password

--- Eureka/PhotoAppApiUsers/src/main/java/com/curse/app/ws/photoapp/api/users/service/UsersServiceImpl.java
+++ Eureka/PhotoAppApiUsers/src/main/java/com/curse/app/ws/photoapp/api/users/service/UsersServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.curse.app.ws.photoapp.api.users.data.UserEntity;
@@ -15,11 +16,13 @@ import com.curse.app.ws.photoapp.api.users.shared.UserDto;
 public class UsersServiceImpl implements UsersService {
 	
 	UsersRepository usersRepository;
+	BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	@Autowired
-	public UsersServiceImpl(UsersRepository usersRepository)
+	public UsersServiceImpl(UsersRepository usersRepository, BCryptPasswordEncoder bCryptPasswordEncoder)
 	{
 		this.usersRepository = usersRepository;
+		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
 	}
 
 	@Override
@@ -27,15 +30,18 @@ public class UsersServiceImpl implements UsersService {
 		// TODO Auto-generated method stub
 		
 		userDetails.setUserId(UUID.randomUUID().toString());
+		userDetails.setEncryptedPassword(bCryptPasswordEncoder.encode(userDetails.getPassword()));
+		
 		ModelMapper modelMapper = new ModelMapper();
 		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 		
 		UserEntity userEntity = modelMapper.map(userDetails, UserEntity.class);
-		userEntity.setEncryptedPassword("test");
 		
 		usersRepository.save(userEntity);
 		
-		return null;
+		UserDto returnValue = modelMapper.map(userEntity, UserDto.class);
+		
+		return returnValue;
 	}
 
 }
--- Eureka/PhotoAppApiUsers/src/main/java/com/curse/app/ws/photoapp/api/users/ui/controllers/UsersController.java
+++ Eureka/PhotoAppApiUsers/src/main/java/com/curse/app/ws/photoapp/api/users/ui/controllers/UsersController.java
@@ -6,6 +6,9 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.curse.app.ws.photoapp.api.users.service.*;
 import com.curse.app.ws.photoapp.api.users.shared.*;
 import com.curse.app.ws.photoapp.api.users.ul.model.CreateUserRequestModel;
+import com.curse.app.ws.photoapp.api.users.ul.model.CreateUserResponseModel;
 
 @RestController
 @RequestMapping("/users")
@@ -32,14 +36,21 @@ public class UsersController {
 		return "Working on port "+ env.getProperty("local.server.port");
 	}
 
-	@PostMapping
-	public String CreateUser(@Valid @RequestBody CreateUserRequestModel userDetails)
+	@PostMapping(
+			consumes = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE },
+			produces = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE }
+			)
+	public ResponseEntity<CreateUserResponseModel> CreateUser(@Valid @RequestBody CreateUserRequestModel userDetails)
 	{
 		ModelMapper modelMapper = new ModelMapper();
 		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 		
 		UserDto userDto = modelMapper.map(userDetails, UserDto.class);
-		usersService.createUser(userDto);
-		return "Create user has called";
+		
+		UserDto createdUser = usersService.createUser(userDto);
+		
+		CreateUserResponseModel returnValue = modelMapper.map(createdUser, CreateUserResponseModel.class);
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(returnValue);
 	}
 }
