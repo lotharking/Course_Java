6c1fc8213a3f704aba78aadee1e0c631ece09501
Andres Vallecilla <andresfelipe083195@hotmail.com>
2022-05-26 20:08:19 -0500

    [change] add filters and path configuration security

---
+++ Eureka/PhotoAppApiGateway/src/main/java/com/curse/app/ws/photoapp/api/gateway/AuthorizationHeaderFilter.java
@@ -0,0 +1,20 @@
+package com.curse.app.ws.photoapp.api.gateway;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory {
+	
+	public static class Config {
+		//Put configuration properties here
+	}
+
+	@Override
+	public GatewayFilter apply(Object config) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
