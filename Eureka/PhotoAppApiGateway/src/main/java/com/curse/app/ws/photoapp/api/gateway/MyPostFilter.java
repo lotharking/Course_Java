e611f5fc3a90f1ef378f405c2371fe93518a8abc
Andres Vallecilla <andresfelipe083195@hotmail.com>
2022-06-02 21:03:38 -0500

    [change] pre and post filter

---
+++ Eureka/PhotoAppApiGateway/src/main/java/com/curse/app/ws/photoapp/api/gateway/MyPostFilter.java
@@ -0,0 +1,25 @@
+package com.curse.app.ws.photoapp.api.gateway;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class MyPostFilter implements GlobalFilter {
+
+	final Logger logger = LoggerFactory.getLogger(MyPreFilter.class);
+
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+		// TODO Auto-generated method stub
+		return chain.filter(exchange).then(Mono.fromRunnable(()-> {
+			logger.info("Global post executed filter...");
+		}));
+	}
+
+}
