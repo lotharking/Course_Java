92b4f742fb3699e2bc1acb6747e3649745ec9575
Andres Vallecilla <andresfelipe083195@hotmail.com>
2022-07-18 23:34:19 -0500

    [create] Configuration Feign library for connect two microservices

---
+++ Eureka/PhotoAppApiUsers/src/main/java/com/curse/app/ws/photoapp/api/users/data/AlbumsServiceClient.java
@@ -0,0 +1,17 @@
+package com.curse.app.ws.photoapp.api.users.data;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.curse.app.ws.photoapp.api.users.ul.model.AlbumResponseModel;
+
+@FeignClient(name = "albums-ws")
+public interface AlbumsServiceClient {
+	
+	@GetMapping(path = "/users/{id}/albums")
+	public List<AlbumResponseModel> getAlbums(@PathVariable String id);
+
+}
